745b813f4de1033a1fb55ddf74e3948eb6034b1a
Steven Bengtson <steven.bengtson@gmail.com>
2019-05-15 15:03:46 -0700

    Remove api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAspX-zMkN04ucfr7z4r7t5ptfzgyEahdk',
+    apiKey: '',
     authDomain: 'creative-chameleon.firebaseapp.com',
     databaseURL: 'https://creative-chameleon.firebaseio.com',
     projectId: 'creative-chameleon',
     storageBucket: 'creative-chameleon.appspot.com',
-    messagingSenderId: '708442655622'
+    messagingSenderId: ''
   }
 };
